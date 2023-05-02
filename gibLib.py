3e3337dd1154d8464c3e9f2a0b499b4dbee83558
Salil Kumar <--replace-all>
2023-05-03 02:06:09 +0545

    Code refactoring

--- gibLib.py
+++ gibLib.py
@@ -53,6 +53,112 @@ def cmd_show_ref(args): return None
 def cmd_tag(args): return None
 
 
+def repo_path(repo, *path):
+    return os.path.join(repo.gitDir, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    """
+        Same as repo_path, but mkdir *path if absent 
+    """
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
+def repo_file(repo, *path, mkdir = False):
+    """Same as repo_path, but create dirname(*path) if absent.
+       For example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") 
+       will create
+            .git/refs/remotes/origin.
+    """
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_create(path):
+    """Create a new repository at path."""
+
+    repo = GitRepository(path, True)
+
+    # First, we make sure the path either doesn't exist or is an
+    # empty dir.
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception ("Not a directory! :" + path)
+        if os.listdir(repo.worktree):
+            raise Exception("Not an empty Directory! :" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository: edit this file 'description' to name the repository.\n")
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+class GitRepository(object):
+    """
+        Git repository Class
+    """
+
+    workTree = None
+    gitDir = None
+    conf = None
+
+    def __init__(self,path,force=False):
+
+        # force = False means that the git repository exist
+        # force = True => override and create a repository
+        self.workTree = path
+        self.gitDir = os.path.join(path,".git")
+
+        if force and (os.path.isdir(self.gitDir) == False):
+            raise Exception("Not a git repository" + path)
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception('Configuration file is missing on path: ' + cf)
+
+        if not force:
+            vers = int(self.conf.get("core","repositoryformatversion"))
+
+            if vers != 0:
+                raise Exception('Unsupported repositoryformatversion :'+ vers)
+        
+
+
 def main(argv=sys.argv[1:]):
     args = argParser.parse_args(argv)
 
